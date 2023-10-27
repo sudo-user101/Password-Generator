e641cdec3e541c56d7c53bfa3f17cd41472ec367
user <148644508+sudo-user101@users.noreply.github.com>
2023-10-27 11:15:19 +0200

    Add files via upload

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,19 @@
 import random
 from string import digits, ascii_letters, punctuation
 
-def generate_password(length):
-    all_chars = digits + ascii_letters + punctuation
-    password = ''.join(random.choice(all_chars) for _ in range(length))
-    return password
+def pg(length):
+    code = digits + ascii_letters + punctuation
+    results = "".join(random.choice(code) for _ in range(length))
+    return results
 
-user_input = int(input("Enter Length of password: "))
-print("Generated password:", generate_password(user_input))
+try:
+    user_input = input("Enter Length of password:")
+    if user_input.strip() == "":
+        raise ValueError("Input cannot be blank")
+    else:
+      print(pg(user))
+
+except ValueError as e:
+    print("Error:", e)
+except Exception as e:
+    print("Error:", e)
